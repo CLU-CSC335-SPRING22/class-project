a2b021aa129dde54273e46ae2eae873842caca61
CLU-cg <cynthiagutierrez@callutheran.edu>
2022-04-24 22:41:26 -0700

    Added EchoMultiServer

--- Server/EchoMultiServer.java
+++ Server/EchoMultiServer.java
@@ -1,7 +1,16 @@
 package Server;
 import java.net.*;
+import java.io.*;
+
 
 public class EchoMultiServer {
     private ServerSocket serverSocket;
 
-}
+    public void start(int port) throws IOException{
+        serverSocket = new ServerSocket(port);
+
+
+    }
+
+    }
+
